8e5b6bd99bde12157559228740b7ec130032dac9
dac1995 <diegoach1995@gmail.com>
2021-11-05 12:39:55 +0100

    Prueba de conexion
    exito

--- Proyecto/Proyecto/Program.cs
+++ Proyecto/Proyecto/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SQLite;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -16,7 +17,43 @@ namespace Proyecto
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+            using (var connection = new SQLiteConnection("Data Source=C:\\Users\\estudiante\\Documents\\GitHub\\Proyecto\\Datos.db"))
+            {
+                connection.Open();
+
+                var command = connection.CreateCommand();
+                command.CommandText =
+                @"
+                SELECT Usuario
+                FROM Usuarios
+                WHERE Usuario = $id
+                ";
+                command.Parameters.AddWithValue("$id", "J");
+
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        var name = reader.GetString(0);
+
+                        Console.WriteLine($"Hello, {name}!");
+                    }
+                }
+            }
             Application.Run(new Form1());
+            
         }
+
+
     }
+
+    class Usuarios
+    {
+        string Usuario;
+        bool Conduce;
+        int NDiasCond;
+        int Entrada;
+        int Salida;
+    }
+
 }
