162b58876c8bcda93d1d0ca89364d17efc6b4841
dac1995 <diegoach1995@gmail.com>
2022-04-22 10:39:36 +0200

    Littlefix
    Pequeño arreglo de los textos

--- Proyecto/Proyecto/Viajes.Designer.cs
+++ Proyecto/Proyecto/Viajes.Designer.cs
@@ -63,22 +63,20 @@ namespace Proyecto
             this.tabControl1.Controls.Add(this.Miercoles);
             this.tabControl1.Controls.Add(this.Jueves);
             this.tabControl1.Controls.Add(this.Viernes);
-            this.tabControl1.Location = new System.Drawing.Point(60, 27);
-            this.tabControl1.Margin = new System.Windows.Forms.Padding(4);
+            this.tabControl1.Location = new System.Drawing.Point(45, 22);
             this.tabControl1.Multiline = true;
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(818, 446);
+            this.tabControl1.Size = new System.Drawing.Size(614, 362);
             this.tabControl1.TabIndex = 0;
             // 
             // Lunes
             // 
             this.Lunes.Controls.Add(this.dataL);
-            this.Lunes.Location = new System.Drawing.Point(4, 25);
-            this.Lunes.Margin = new System.Windows.Forms.Padding(4);
+            this.Lunes.Location = new System.Drawing.Point(4, 22);
             this.Lunes.Name = "Lunes";
-            this.Lunes.Padding = new System.Windows.Forms.Padding(4);
-            this.Lunes.Size = new System.Drawing.Size(810, 417);
+            this.Lunes.Padding = new System.Windows.Forms.Padding(3, 3, 3, 3);
+            this.Lunes.Size = new System.Drawing.Size(606, 336);
             this.Lunes.TabIndex = 0;
             this.Lunes.Text = "Lunes";
             this.Lunes.UseVisualStyleBackColor = true;
@@ -88,22 +86,22 @@ namespace Proyecto
             this.dataL.AllowUserToAddRows = false;
             this.dataL.AllowUserToDeleteRows = false;
             this.dataL.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataL.Location = new System.Drawing.Point(25, 14);
+            this.dataL.Location = new System.Drawing.Point(19, 11);
+            this.dataL.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.dataL.Name = "dataL";
             this.dataL.ReadOnly = true;
             this.dataL.RowHeadersWidth = 51;
             this.dataL.RowTemplate.Height = 24;
-            this.dataL.Size = new System.Drawing.Size(760, 389);
+            this.dataL.Size = new System.Drawing.Size(570, 316);
             this.dataL.TabIndex = 1;
             // 
             // Martes
             // 
             this.Martes.Controls.Add(this.dataM);
-            this.Martes.Location = new System.Drawing.Point(4, 25);
-            this.Martes.Margin = new System.Windows.Forms.Padding(4);
+            this.Martes.Location = new System.Drawing.Point(4, 22);
             this.Martes.Name = "Martes";
-            this.Martes.Padding = new System.Windows.Forms.Padding(4);
-            this.Martes.Size = new System.Drawing.Size(810, 417);
+            this.Martes.Padding = new System.Windows.Forms.Padding(3, 3, 3, 3);
+            this.Martes.Size = new System.Drawing.Size(606, 336);
             this.Martes.TabIndex = 1;
             this.Martes.Text = "Martes";
             this.Martes.UseVisualStyleBackColor = true;
@@ -113,22 +111,22 @@ namespace Proyecto
             this.dataM.AllowUserToAddRows = false;
             this.dataM.AllowUserToDeleteRows = false;
             this.dataM.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataM.Location = new System.Drawing.Point(25, 14);
+            this.dataM.Location = new System.Drawing.Point(19, 11);
+            this.dataM.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.dataM.Name = "dataM";
             this.dataM.ReadOnly = true;
             this.dataM.RowHeadersWidth = 51;
             this.dataM.RowTemplate.Height = 24;
-            this.dataM.Size = new System.Drawing.Size(760, 389);
+            this.dataM.Size = new System.Drawing.Size(570, 316);
             this.dataM.TabIndex = 1;
             // 
             // Miercoles
             // 
             this.Miercoles.Controls.Add(this.dataX);
-            this.Miercoles.Location = new System.Drawing.Point(4, 25);
-            this.Miercoles.Margin = new System.Windows.Forms.Padding(4);
+            this.Miercoles.Location = new System.Drawing.Point(4, 22);
             this.Miercoles.Name = "Miercoles";
-            this.Miercoles.Padding = new System.Windows.Forms.Padding(4);
-            this.Miercoles.Size = new System.Drawing.Size(810, 417);
+            this.Miercoles.Padding = new System.Windows.Forms.Padding(3, 3, 3, 3);
+            this.Miercoles.Size = new System.Drawing.Size(606, 336);
             this.Miercoles.TabIndex = 2;
             this.Miercoles.Text = "Miercoles";
             this.Miercoles.UseVisualStyleBackColor = true;
@@ -138,22 +136,22 @@ namespace Proyecto
             this.dataX.AllowUserToAddRows = false;
             this.dataX.AllowUserToDeleteRows = false;
             this.dataX.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataX.Location = new System.Drawing.Point(25, 15);
+            this.dataX.Location = new System.Drawing.Point(19, 12);
+            this.dataX.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.dataX.Name = "dataX";
             this.dataX.ReadOnly = true;
             this.dataX.RowHeadersWidth = 51;
             this.dataX.RowTemplate.Height = 24;
-            this.dataX.Size = new System.Drawing.Size(760, 389);
+            this.dataX.Size = new System.Drawing.Size(570, 316);
             this.dataX.TabIndex = 1;
             // 
             // Jueves
             // 
             this.Jueves.Controls.Add(this.dataJ);
-            this.Jueves.Location = new System.Drawing.Point(4, 25);
-            this.Jueves.Margin = new System.Windows.Forms.Padding(4);
+            this.Jueves.Location = new System.Drawing.Point(4, 22);
             this.Jueves.Name = "Jueves";
-            this.Jueves.Padding = new System.Windows.Forms.Padding(4);
-            this.Jueves.Size = new System.Drawing.Size(810, 417);
+            this.Jueves.Padding = new System.Windows.Forms.Padding(3, 3, 3, 3);
+            this.Jueves.Size = new System.Drawing.Size(606, 336);
             this.Jueves.TabIndex = 3;
             this.Jueves.Text = "Jueves";
             this.Jueves.UseVisualStyleBackColor = true;
@@ -163,22 +161,22 @@ namespace Proyecto
             this.dataJ.AllowUserToAddRows = false;
             this.dataJ.AllowUserToDeleteRows = false;
             this.dataJ.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataJ.Location = new System.Drawing.Point(25, 14);
+            this.dataJ.Location = new System.Drawing.Point(19, 11);
+            this.dataJ.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.dataJ.Name = "dataJ";
             this.dataJ.ReadOnly = true;
             this.dataJ.RowHeadersWidth = 51;
             this.dataJ.RowTemplate.Height = 24;
-            this.dataJ.Size = new System.Drawing.Size(760, 389);
+            this.dataJ.Size = new System.Drawing.Size(570, 316);
             this.dataJ.TabIndex = 1;
             // 
             // Viernes
             // 
             this.Viernes.Controls.Add(this.dataV);
-            this.Viernes.Location = new System.Drawing.Point(4, 25);
-            this.Viernes.Margin = new System.Windows.Forms.Padding(4);
+            this.Viernes.Location = new System.Drawing.Point(4, 22);
             this.Viernes.Name = "Viernes";
-            this.Viernes.Padding = new System.Windows.Forms.Padding(4);
-            this.Viernes.Size = new System.Drawing.Size(810, 417);
+            this.Viernes.Padding = new System.Windows.Forms.Padding(3, 3, 3, 3);
+            this.Viernes.Size = new System.Drawing.Size(606, 336);
             this.Viernes.TabIndex = 4;
             this.Viernes.Text = "Viernes";
             this.Viernes.UseVisualStyleBackColor = true;
@@ -188,30 +186,33 @@ namespace Proyecto
             this.dataV.AllowUserToAddRows = false;
             this.dataV.AllowUserToDeleteRows = false;
             this.dataV.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataV.Location = new System.Drawing.Point(25, 14);
+            this.dataV.Location = new System.Drawing.Point(19, 11);
+            this.dataV.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.dataV.Name = "dataV";
             this.dataV.ReadOnly = true;
             this.dataV.RowHeadersWidth = 51;
             this.dataV.RowTemplate.Height = 24;
-            this.dataV.Size = new System.Drawing.Size(760, 389);
+            this.dataV.Size = new System.Drawing.Size(570, 316);
             this.dataV.TabIndex = 1;
             this.dataV.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataV_CellContentClick);
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(830, 480);
+            this.button1.Location = new System.Drawing.Point(544, 390);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(171, 40);
+            this.button1.Size = new System.Drawing.Size(206, 32);
             this.button1.TabIndex = 1;
-            this.button1.Text = "Actualizar datos";
+            this.button1.Text = "Avanzar semana";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(12, 505);
+            this.button2.Location = new System.Drawing.Point(9, 410);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(171, 40);
+            this.button2.Size = new System.Drawing.Size(128, 32);
             this.button2.TabIndex = 2;
             this.button2.Text = "Restaurar datos";
             this.button2.UseVisualStyleBackColor = true;
@@ -219,9 +220,10 @@ namespace Proyecto
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(898, 66);
+            this.button3.Location = new System.Drawing.Point(674, 54);
+            this.button3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(103, 108);
+            this.button3.Size = new System.Drawing.Size(77, 88);
             this.button3.TabIndex = 3;
             this.button3.Text = "Gestionar datos";
             this.button3.UseVisualStyleBackColor = true;
@@ -229,14 +231,13 @@ namespace Proyecto
             // 
             // Viajes
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1013, 557);
+            this.ClientSize = new System.Drawing.Size(760, 453);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.tabControl1);
-            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Viajes";
             this.Text = "Viajes";
             this.Load += new System.EventHandler(this.Viajes_Load);
